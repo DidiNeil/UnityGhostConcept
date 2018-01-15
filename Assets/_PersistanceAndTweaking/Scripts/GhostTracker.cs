72ef68fc48c284a55e1b3cf097c876e3f4e2106e
JeromeJ <jerome.jamart.42@gmail.com>
2018-01-15 15:32:53 +0100

    First commit

---
+++ Assets/_PersistanceAndTweaking/Scripts/GhostTracker.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTracker : DualBehaviour
+{
+    #region Public Members
+
+    public GameObject m_prefab;
+    public TrackingData m_tracker;
+
+    public GameObject m_player;
+    public float m_speed = .5f;
+
+    public int i = -1;
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    protected override void Awake()
+    {
+        m_player = Instantiate(m_prefab);
+
+        count = m_tracker.positions.Count;
+
+        if (count > 0)
+            i = 0;
+        else
+            i = -1;
+    }
+
+    private void FixedUpdate()
+    {
+        if (i < 0)
+        {
+            Vector3 newPos = m_player.transform.position;
+
+            if ( Input.GetKey("down"))
+                newPos.y -= m_speed;
+            else if (Input.GetKey("up"))
+                newPos.y += m_speed;
+            else if (Input.GetKey("right"))
+                newPos.x += m_speed;
+            else if (Input.GetKey("left"))
+                newPos.x -= m_speed;
+
+            m_player.transform.position = newPos;
+
+            m_tracker.positions.Add(newPos);
+        }
+        else if (i < count)
+        {
+            m_player.transform.position = m_tracker.positions[i];
+
+            i++;
+        }
+        else
+            m_tracker.positions.Clear();
+    }
+
+    #endregion
+
+    #region Class Methods
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+
+    private int count;
+
+    #endregion
+}
---
+++ Assets/_PersistanceAndTweaking/Scripts/TrackingData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TrackingData : ScriptableObject
+{
+    #region Public Members
+
+    public List<Vector3> positions;
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    #endregion
+
+    #region Class Methods
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
